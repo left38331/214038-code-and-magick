fbcf90781a4387cf48c182db14fba274189bca0e
Egor <left38331@mail.ru>
2017-03-30 15:44:22 +0300

    улучшение кода

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,23 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
+  var i;
+  var max = 0;
+  var maxTimeFunction = function () {   //функция записи времени и выбора макс времени
+    if (times[i] > max) {
+      max = times[i];
+    }
+  };
+  var colorNames = function () {  //функция выбора цвета
+    var minimum = 0.1;
+    var maximum = 1;
+    var randomNumber = Math.random() * (maximum - minimum) + minimum;
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(17, 9, 255, ' + randomNumber + ' )';
+    }
+  };
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
   ctx.fillStyle = 'white';
@@ -10,41 +27,20 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
-  var max = -1;
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
-    }
-  }
   var histogramHeight = 150;
-  var step = histogramHeight / (max - 0);
-  ctx.save();
-  ctx.translate(460, 240);
-  ctx.rotate(180 * Math.PI / 180);
   var barWidth = 40;
   var indent = 50;
-  var initialX = 0;
+  var initialXHistogram = 150;
   var initialY = 275;
-  var initialYHistogram = -20;
-  var initialYTimes = 100;
-  for (i = 0; i < times.length; i++) {
-    var minimum = 0.1;
-    var maximum = 1;
-    var randomNumber = Math.random() * (maximum - minimum) + minimum;
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(17, 9, 255, ' + randomNumber + ' )';
-    }
-    ctx.fillRect((barWidth + indent) * i, initialYHistogram, barWidth, times[i] * step);
+  for (i = 0; i < times.length; i++) {       //нахождение максимального времени
+    maxTimeFunction();
   }
-  ctx.restore();
-  ctx.save();
-  ctx.translate(420, 0);
+  var step = histogramHeight / (max);        //определение шага
   for (i = 0; i < times.length; i++) {
-    ctx.fillText(names[i], initialX - (barWidth + indent) * i, initialY);
-    ctx.fillText(Math.round(times[i]), initialX - (barWidth + indent) * i, initialYTimes);
+    colorNames();
+    ctx.fillRect((initialXHistogram + (barWidth + indent) * i), (260 - (times[i] * step)), barWidth, times[i] * step); //отрисовка гистограммы
+    ctx.fillStyle = 'black';
+    ctx.fillText(names[i], (initialXHistogram + (barWidth + indent) * i), initialY);  //вывод имени
+    ctx.fillText(Math.round(times[i]), (initialXHistogram + (barWidth + indent) * i), 250 - (times[i] * step));  //вывод времени
   }
-  ctx.restore();
 };
