6297ad1aaa5853588bc29eb769bdce873703d255
Egor <left38331@mail.ru>
2017-04-02 12:38:22 +0300

    задание 3

---
+++ js/setup.js
@@ -0,0 +1,50 @@
+'use strict';
+
+document.querySelector('.setup').classList.remove('hidden');
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarVizardTemplate = document.querySelector('#similar-wizard-template').content;
+var randomFuncName = function () {
+  var firstName = ['Иван ', 'Хуан Себастьян ', 'Мария ', 'Кристоф ', 'Виктор ', 'Юлия ', 'Люпита ', 'Вашингтон '];
+  var lastName = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  var randomFirstName = Math.floor(Math.random() * firstName.length);
+  var randomLastName = Math.floor(Math.random() * lastName.length);
+  var randomFirstNameString = firstName[randomFirstName];
+  var randomLastNameString = lastName[randomLastName];
+  var fullName = randomFirstNameString + randomLastNameString;
+  return fullName;
+};
+var randomFuncColor = function () {
+  var colorJacket = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var randomColorJacket = Math.floor(Math.random() * colorJacket.length);
+  var selectedColorJacket = colorJacket[randomColorJacket];
+  return selectedColorJacket;
+};
+var randomFuncOptic = function () {
+  var colorOptic = ['black', 'red', 'blue', 'yellow', 'green'];
+  var randomColorOptic = Math.floor(Math.random() * colorOptic.length);
+  var selectedColorOptic = colorOptic[randomColorOptic];
+  return selectedColorOptic;
+};
+var randomArray = [
+  {name: randomFuncName(), coatColor: randomFuncColor(), eyesColor: randomFuncOptic()},
+  {name: randomFuncName(), coatColor: randomFuncColor(), eyesColor: randomFuncOptic()},
+  {name: randomFuncName(), coatColor: randomFuncColor(), eyesColor: randomFuncOptic()},
+  {name: randomFuncName(), coatColor: randomFuncColor(), eyesColor: randomFuncOptic()}
+];
+var renderWizard = function (randomArray) {
+  var wizardElement = similarVizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = randomArray.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = randomArray.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = randomArray.eyesColor;
+  return wizardElement;
+};
+var createFunc = function () {
+  var i;
+  var fragment = document.createDocumentFragment();
+  for (i = 0; i < randomArray.length; i++) {
+    fragment.appendChild(renderWizard(randomArray[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
+createFunc();
+document.querySelector('.setup-similar').classList.remove('hidden');
