fb9cd3b34727c9c0e0dea4148faead5b38a32a04
Egor <left38331@mail.ru>
2017-04-19 08:13:57 +0300

    правки

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,7 +1,7 @@
 'use strict';
 
-window.coloriseElement = (function(element, colors, callback) {
-  element.addEventListener('click', function() {
+window.coloriseElement = (function (element, colors, callback) {
+  element.addEventListener('click', function () {
     // Возвращает рандомный элемет массива
     var randomColor = colors[Math.floor(Math.random() * colors.length)];
     callback(element, randomColor);
