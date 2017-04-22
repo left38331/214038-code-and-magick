6089183179957c1d613f1f36fa179daabed831de
Egor <left38331@mail.ru>
2017-04-22 17:03:58 +0300

    правки

--- js/load.js
+++ js/load.js
@@ -2,11 +2,9 @@
 
 (function () {
   var URL = 'https://intensive-javascript-server-pedmyactpq.now.sh/code-and-magick/data';
-  
   window.load = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-    
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
         onSuccess(xhr.response);
@@ -20,9 +18,7 @@
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
-    
     xhr.timeout = 10000; // 10s
-    
     xhr.open('GET', URL);
     xhr.send();
   };
--- js/setup.js
+++ js/setup.js
@@ -15,28 +15,6 @@ window.utils = (function () {
 window.isCreateHeroes = (function () {
   var similarListElement = document.querySelector('.setup-similar-list');
   var similarVizardTemplate = document.querySelector('#similar-wizard-template').content;
-  var randomFuncName = function () {
-    var firstName = ['Иван ', 'Хуан Себастьян ', 'Мария ', 'Кристоф ', 'Виктор ', 'Юлия ', 'Люпита ', 'Вашингтон '];
-    var lastName = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-    var randomFirstName = Math.floor(Math.random() * firstName.length);
-    var randomLastName = Math.floor(Math.random() * lastName.length);
-    var randomFirstNameString = firstName[randomFirstName];
-    var randomLastNameString = lastName[randomLastName];
-    var fullName = randomFirstNameString + randomLastNameString;
-    return fullName;
-  };
-  var randomFuncColor = function () {
-    var colorJacket = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-    var randomColorJacket = Math.floor(Math.random() * colorJacket.length);
-    var selectedColorJacket = colorJacket[randomColorJacket];
-    return selectedColorJacket;
-  };
-  var randomFuncOptic = function () {
-    var colorOptic = ['black', 'red', 'blue', 'yellow', 'green'];
-    var randomColorOptic = Math.floor(Math.random() * colorOptic.length);
-    var selectedColorOptic = colorOptic[randomColorOptic];
-    return selectedColorOptic;
-  };
   var wizardCoatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var wizardEyesColors = ['black', 'red', 'blue', 'yellow', 'grenn'];
   var wizardFireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
@@ -50,9 +28,9 @@ window.isCreateHeroes = (function () {
   var successHandler = function (wizards) {
     var fragment = document.createDocumentFragment();
     // определяем сколько волшебников нам сервер может передать
-    var aaa = Object.keys(wizards).length;
+    var numberWizards = Object.keys(wizards).length;
     var min = 0;
-    var max = 13;
+    var max = numberWizards - 4;
     // определяем, с какого по счету  мага добавятсья еще 3 мага
     var randomNumber = Math.round(Math.random() * (max - min) + min);
     for (var i = randomNumber; i < randomNumber + 4; i++) {
@@ -67,9 +45,9 @@ window.isCreateHeroes = (function () {
     node.style.left = 0;
     node.style.right = 0;
     node.style.fontSize = '30px';
-    node.textContent = errorMessage; 
+    node.textContent = errorMessage;
     document.body.insertAdjacentElement('afterbegin', node);
-  }
+  };
   window.load(successHandler, errorHandler);
   return {
     arrayColorCoat: wizardCoatColors,
